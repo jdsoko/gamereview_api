9ecdfd4439331caaff614767ba55e387f078a243
John Sokolowski <johndsoko@gmail.com>
2026-06-29 17:48:40 -0500

    update allowed origins for CORS

--- src/app.js
+++ src/app.js
@@ -1,54 +1,61 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const reviewsRouter = require('./reviews/reviews-router')
-const gamesRouter = require('./games/games-router')
-const usersRouter = require('./users/users-router')
-const authRouter = require('./auth/auth-router')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const reviewsRouter = require("./reviews/reviews-router");
+const gamesRouter = require("./games/games-router");
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
-const app = express()
+const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-    skip: () => NODE_ENV === 'test',
-}))
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test",
+  }),
+);
 
-const allowedOrigins = ['http://localhost:3000',
-                  'https://gamereview.now.sh/games'];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://gamereview.vercel.app/",
+];
 
-app.use(helmet())
-app.use(cors({
-    origin: function(origin, callback){
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'The CORS policy for this site does not ' +
-                  'allow access from the specified Origin.';
+app.use(helmet());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
         return callback(new Error(msg), false);
       }
       return callback(null, true);
-    }
-  }));
+    },
+  }),
+);
 
-app.use('/api/reviews', reviewsRouter)
-app.use('/api/games', gamesRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/auth', authRouter)
+app.use("/api/reviews", reviewsRouter);
+app.use("/api/games", gamesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
